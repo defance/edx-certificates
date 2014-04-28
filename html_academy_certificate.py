135b3315cc2152d388b9a590cc0d9274d686f4c3
Dmitry Ivanyushin <defance@gmail.com>
2014-04-28 18:39:11 +0400

    Added HTMLAcademy certificate realization

---
+++ html_academy_certificate.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import certificate
+import globals
+
+
+class HTMLAcademyCertificate(certificate.Certificate):
+
+    _COURSE_ID = 'ITMO/x0002.00/2014_02'
+    _COURSE_NAME = 'Создание веб-интерфейсов с помощью HTML и CSS'
+    _SOURCE = globals.CERTIFICATE
+
+    def __init__(self, student_obj, secret=globals.SECRET, base_path='./', output_prefix='./'):
+        super(HTMLAcademyCertificate, self).__init__(
+            student_obj,
+            self._SOURCE,
+            self._COURSE_ID,
+            self._COURSE_NAME,
+            secret=secret,
+            base_path=base_path,
+            output_prefix=output_prefix
+        )
